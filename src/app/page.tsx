97c3f25cc061f187797f896815841649643cb8ea
rishi <rishiraj727909@gmail.com>
2024-01-30 02:27:46 +0530

    defined global.css with ui.shadcn.com

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Next.js!</h1>
-      <p>
-        Get started by editing{" "}
-        <code>pages/index.js</code>
-      </p>
-      <Image src="/favicon.ico" alt="logo" width={100} height={100} />
-    </div>
+    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <div className="mx-auto mb-4 flex max-w-fit items-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+        <p className="text-sm font-semibold text-gray-700">
+          Pdfwizard has just been startwd making now 
+        </p>
+      </div>
+    </MaxWidthWrapper>
   );
 }
-  
